f46efd73a58f1703930f6dc637b64db0e0bd60ea
Larina Vasilina <larina.vasilina@list.ru>
2026-05-02 15:13:08 +0700

    Initial infrastructure (docker, microservices)

---
+++ trip-service/src/test/java/com/taxi/trip_service/TripServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taxi.trip_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
