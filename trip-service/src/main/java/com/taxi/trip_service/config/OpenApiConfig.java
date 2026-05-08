736498db7e09602eddc4da6044a7d4aa183ba0e8
Larina Vasilina <larina.vasilina@list.ru>
2026-05-08 11:52:21 +0700

    Add swagger for user- and trip- service

---
+++ trip-service/src/main/java/com/taxi/trip_service/config/OpenApiConfig.java
@@ -0,0 +1,35 @@
+package com.taxi.trip_service.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Trip Service API")
+                        .version("1.0")
+                        .description("Taxi Trip Management Service")
+                )
+                .addSecurityItem(new SecurityRequirement()
+                        .addList("bearerAuth")
+                )
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth",
+                                new SecurityScheme()
+                                        .name("bearerAuth")
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                        )
+                );
+    }
+}
